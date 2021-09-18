51cb2ac840619d47b7d68d81cbea5f026dc28bd4
Erinxon <30541728+Erinxon@users.noreply.github.com>
2021-09-17 22:30:47 -0400

    Add OnDestroy

--- AppEmpleados/src/app/Shared/error/error.component.ts
+++ AppEmpleados/src/app/Shared/error/error.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ErrorModel } from 'src/app/models/ErrorModel';
 import { ErrorService } from 'src/app/services/error.service';
 
@@ -8,18 +9,27 @@ import { ErrorService } from 'src/app/services/error.service';
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent implements OnInit, OnDestroy {
+  susbcription!: Subscription;
   error!: ErrorModel;
+
   constructor(private errorService: ErrorService, private ruter: Router) { }
+
 
   ngOnInit(): void {
     this.getError();
   }
 
+  ngOnDestroy(): void {
+    this.susbcription.unsubscribe();
+  }
+
+
   getError(): void {
+    this.susbcription.add(
     this.errorService.getError().subscribe(e => {
       this.error = e;
-    })
+    }));
   }
 
   setError(){
--- AppEmpleados/src/app/Shared/loading/loading.component.ts
+++ AppEmpleados/src/app/Shared/loading/loading.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ErrorModel } from 'src/app/models/ErrorModel';
 import { LoadingService } from 'src/app/services/loading.service';
 
@@ -7,14 +8,23 @@ import { LoadingService } from 'src/app/services/loading.service';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
 })
-export class LoadingComponent implements OnInit {
+export class LoadingComponent implements OnInit, OnDestroy {
+  susbcription!: Subscription;
   isLoanding: boolean = false;
+
   constructor(private loandingService: LoadingService) { }
 
   ngOnInit(): void {
-    this.loandingService.getLoanding().subscribe((loanding) => {
-      this.isLoanding = loanding;
-    });
+    this.susbcription.add(
+      this.loandingService.getLoanding().subscribe((loanding) => {
+        this.isLoanding = loanding;
+      })
+    );
   }
+
+  ngOnDestroy(): void {
+    this.susbcription.unsubscribe();
+  }
+
 
 }
